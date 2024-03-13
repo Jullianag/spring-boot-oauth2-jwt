61f911f25e2d7de78b059824765b6b6d1057a82d
Julliana <juh_g@hotmail.com>
2024-03-13 14:45:20 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$hLBgGSB10MOql7P1a3M3A.5WEVMgWEuDhbrL93BK13RxvZQV/F8FG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$hLBgGSB10MOql7P1a3M3A.5WEVMgWEuDhbrL93BK13RxvZQV/F8FG');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
